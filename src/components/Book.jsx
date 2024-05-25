fa4846361caae585a4f724174eef4f855b6bb32c
rinku <rinkusarkar353@gmail.com>
2024-05-24 22:15:53 -0400

    made some changes in responsive

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,46 +4,37 @@ import Rating from "./Rating";
 const Book = ({ book, onDelete, onRate }) => {
   if (!book) {
     return null;
-  } else {
-    // console.log("check", book);
   }
+
   const rating = book[0].rating;
   const { title, author, coverUrl, isbn, id } = book[0];
-  // console.log("ssss", book[0], book, book[0].rating);
-  // console.log("ss2", book[0].rating)
+
   return (
-    <li className="bg-black max-h-50 max-w-screen flex p-4">
+    <li style={{maxWidth: "400px"}} className="bg-black max-h-50 flex flex-col md:flex-row p-4 items-center ">
       <img
         src={coverUrl}
         alt={title}
-        style={{ height: "150px", width: "150px" }}
+        className="h-36 w-36 md:h-48 md:w-48 mb-4 md:mb-0"
       />
 
-      <div
-        style={{
-          maxWidth: "200px",
-          marginLeft: "0px",
-        }}
-      >
-        <div className="ml-2 font-bold text-lg text-white mb-3">{title}</div>
-        <div className="ml-2  text-white">Author: {author}</div>
-        {isbn && (
-          <div className="ml-2 text-white mb-1">ISBN: {isbn}</div>
-        )}
-        <div className=" ml-2 text-lg text-white mb-2">
+      <div className="md:ml-4 flex flex-col justify-between w-full">
+        <div className="text-lg font-bold text-white mb-3">{title}</div>
+        <div className="text-white">Author: {author}</div>
+        {isbn && <div className="text-white mb-1">ISBN: {isbn}</div>}
+        <div className="text-lg text-white mb-2 flex items-center">
           <Rating
             value={rating}
             color="#f8e825"
-            size="12px"
+            size="20px"
             onRatingChange={(value) => onRate(id, value)}
           />{" "}
         </div>
         <button
           onClick={() => onDelete(id)}
-          className="bg-red-500 min-w-40 ml-2 px-3 text-white rounded-sm"
+          className="bg-red-500 w-full md:w-auto mt-2 md:mt-0 px-3 py-2 text-white rounded-sm"
         >
           Delete
-        </button>{" "}
+        </button>
       </div>
     </li>
   );
